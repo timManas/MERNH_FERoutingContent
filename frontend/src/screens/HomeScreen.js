17719f54003752c2a0c05a0ac81faf4b63f98bbd
TimManas <timothy.romero.manas@gmail.com>
2021-01-26 21:18:00 -0500

    Adding content

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const HomeScreen = () => {
-  return <h1>HOME SCREEN</h1>
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisiut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </p>
+    </>
+  )
 }
 
 export default HomeScreen
